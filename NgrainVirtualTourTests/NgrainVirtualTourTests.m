f7001871819685b26806166e2e3c1613ac64eea7
Shoaib Khan <shoaib@Shoaibs-MacBook-Pro.local>
2015-07-06 15:09:16 -0400

    Initial Commit

---
+++ NgrainVirtualTourTests/NgrainVirtualTourTests.m
@@ -0,0 +1,40 @@
+//
+//  NgrainVirtualTourTests.m
+//  NgrainVirtualTourTests
+//
+//  Created by Shoaib Khan on 2015-07-06.
+//  Copyright (c) 2015 Shoaib Khan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface NgrainVirtualTourTests : XCTestCase
+
+@end
+
+@implementation NgrainVirtualTourTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
